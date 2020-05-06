a84c9edd232279b89062825c589266734ac42523
Simon Galasso <simon.galasso@hotmail.fr>
2020-05-07 01:12:39 +0200

    [SG] Refactor matrix with rulinalg crate wip

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate rand;
+extern crate rulinalg;
 
 use rand::Rng;
+use rulinalg::matrix::Matrix;
 
 /* Math ------------------------------------------ */
 
@@ -8,113 +10,62 @@ fn sigmoid(x: f64) -> f64 {
     return 1.0 / (1.0 + (-x).exp());
 }
 
-/* Matrix ---------------------------------------- */
-
-#[derive(Debug)]
-struct Matrix {
-    values: Vec<Vec<f64>>
-}
-
-impl Matrix { // refactor not self functions and self functions
-    fn new(nb_rows: usize, nb_cols: usize) -> Matrix {
-        Matrix {
-            values: (0..nb_rows).map(|_| (0..nb_cols).map(|_| 0.0).collect()).collect()
-        }
-    }
-
-    fn from_vec(vec: Vec<f64>) -> Matrix { // to refactor with map()
-        let mut m: Matrix = Matrix::new(vec.len(), 1);
-        for i in 0..vec.len() {
-            m.values[i][0] = vec[i];
-        }
-        return m;
-    }
-
-    fn to_vec(&self) -> Vec<f64> { // to refactor with map()
-        let mut v: Vec<f64> = Vec::new();
-        for i in 0..self.values.len() {
-            for j in 0..self.values[i].len() {
-                v.push(self.values[i][j]);
-            }
-        }
-        return v;
-    }
-
-    fn randomize(&mut self) {
-        self.values = (0..self.values.len()).map(|y| (0..self.values[y].len()).map(|_| rand::thread_rng().gen_range(-1.0, 1.0)).collect()).collect()
-    }
-
-    fn add(&mut self, n: f64) {
-        self.values = (0..self.values.len()).map(|y| (0..self.values[y].len()).map(|x| self.values[y][x] + n).collect()).collect()
-    }
-
-    fn scale(&mut self, n: f64) {
-        self.values = (0..self.values.len()).map(|y| (0..self.values[y].len()).map(|x| self.values[y][x] * n).collect()).collect()
-    }
-
-    fn multiply(a: &Matrix, b: &Matrix) -> Matrix { // to refactor with map()
-        let mut new: Matrix = Matrix::new(a.values.len(), b.values[0].len());
-
-        for y in 0..new.values.len() {
-            for x in 0..new.values[y].len() {
-                let mut sum: f64 = 0.0;
-                for i in 0..b.values.len() {
-                    sum += a.values[y][i] * b.values[i][x];
-                }
-                new.values[y][x] = sum;
-            }
-        }
-        return new;
-    }
+fn dsigmoid(y: f64) -> f64 {
+    return y * (1.0 - y);
 }
 
 /* Neural Network -------------------------------- */
 
 struct NeuralNetwork {
-    weights_ih: Matrix,
-    weights_ho: Matrix,
-    bias_h: f64,
-    bias_o: f64
+    weights_ih: Matrix<f64>,
+    weights_ho: Matrix<f64>,
+    bias_h: Matrix<f64>,
+    bias_o: Matrix<f64>
 }
 
 impl NeuralNetwork {
-    fn new(nb_inputs: usize, nb_hidden: usize, nb_outputs: usize) -> NeuralNetwork { // to refactor with map()
-        let mut n = NeuralNetwork {
-            weights_ih: Matrix::new(nb_hidden, nb_inputs),
-            weights_ho: Matrix::new(nb_outputs, nb_hidden),
-            bias_h: 1.0,
-            bias_o: 1.0
-        };
-        n.weights_ih.randomize();
-        n.weights_ho.randomize();
-        return n;
+    fn new(nb_inputs: usize, nb_hidden: usize, nb_outputs: usize) -> NeuralNetwork {
+        NeuralNetwork {
+            weights_ih: Matrix::new(nb_hidden, nb_inputs, (0..nb_hidden * nb_inputs).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
+            weights_ho: Matrix::new(nb_outputs, nb_hidden, (0..nb_outputs * nb_hidden).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
+            bias_h: Matrix::new(nb_hidden, 1, (0..nb_hidden).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
+            bias_o: Matrix::new(nb_outputs, 1, (0..nb_hidden).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>())
+        }
     }
 
-    fn feed_forward(&self, input_vec: Vec<f64>) -> Vec<f64> {
-        let inputs = Matrix::from_vec(input_vec);
-
-        // Generating hidden outputs
-        let mut hidden = Matrix::multiply(&self.weights_ih, &inputs);
-        hidden.add(self.bias_h);
+    fn train(&mut self, inputs: Vec<f64>, targets: Vec<f64>) { 
+        // Feed forward inputs -> hidden
+        let mut hidden = Matrix::product(&self.weights_ih, &inputs);
+        hidden.add(&self.bias_h);
         // Activation function
         hidden.values = (0..hidden.values.len()).map(|y| (0..hidden.values[y].len()).map(|x| sigmoid(hidden.values[y][x])).collect()).collect();
 
-        // Generating output's outputs
-        let mut outputs = Matrix::multiply(&self.weights_ho, &hidden);
-        outputs.add(self.bias_o);
+        // Feed forward hidden -> outputs
+        let mut outputs = Matrix::product(&self.weights_ho, &hidden);
+        outputs.add(&self.bias_o);
         // Activation function
         outputs.values = (0..outputs.values.len()).map(|y| (0..outputs.values[y].len()).map(|x| sigmoid(outputs.values[y][x])).collect()).collect();
 
-        return outputs.to_vec();
+        // Computing the outputs errors
+        let output_errors: Matrix = Matrix::sub(&targets, &outputs);
+
+        //outputs.values = (0..outputs.values.len()).map(|y| (0..outputs.values[y].len()).map(|x| dsigmoid(outputs.values[y][x])).collect()).collect();
+        //outputs.value = Matix::multiply();
+
+        // Computing hidden errors
+        let weights_ho_transposed: Matrix = Matrix::transpose(&self.weights_ho);
+        let hidden_errors: Matrix = Matrix::product(&weights_ho_transposed, &output_errors);
+
+        //println!("{:?}\n{:?}\n{:?}", outputs, targets, output_errors);
     }
 }
 
-/* Functions ------------------------------------- */
+/* Functions ------------------------------------- */ 
 
 fn main() {
-    let neural_network: NeuralNetwork = NeuralNetwork::new(2, 4, 2);
-    let inputs: Vec<f64> = vec!(1.0, 0.0);
+    let mut neuralnet: NeuralNetwork = NeuralNetwork::new(2, 2, 2);
+    let inputs = Matrix::new(2, 1, vec!(1.0, 0.0));
+    let targets = Matrix::new(2, 1, vec!(1.0, 0.0));
 
-    let output: Vec<f64> = neural_network.feed_forward(inputs);
-    println!("{:?}", output);
+    neuralnet.train(inputs, targets);
 }
