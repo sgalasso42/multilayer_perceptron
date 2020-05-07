c72e1467e540693b87c60117c29fd40729f878a7
Simon Galasso <simon.galasso@hotmail.fr>
2020-05-07 11:44:44 +0200

    [SG] Refactor matrix with rulinalg crate done

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ extern crate rand;
 extern crate rulinalg;
 
 use rand::Rng;
-use rulinalg::matrix::Matrix;
+use rulinalg::matrix::{Matrix, BaseMatrixMut, BaseMatrix};
 
 /* Math ------------------------------------------ */
 
@@ -26,37 +26,34 @@ struct NeuralNetwork {
 impl NeuralNetwork {
     fn new(nb_inputs: usize, nb_hidden: usize, nb_outputs: usize) -> NeuralNetwork {
         NeuralNetwork {
-            weights_ih: Matrix::new(nb_hidden, nb_inputs, (0..nb_hidden * nb_inputs).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
-            weights_ho: Matrix::new(nb_outputs, nb_hidden, (0..nb_outputs * nb_hidden).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
-            bias_h: Matrix::new(nb_hidden, 1, (0..nb_hidden).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
-            bias_o: Matrix::new(nb_outputs, 1, (0..nb_hidden).map(|v| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>())
+
+            weights_ih: Matrix::new(nb_hidden, nb_inputs, (0..(nb_hidden * nb_inputs)).map(|_| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
+            weights_ho: Matrix::new(nb_outputs, nb_hidden, (0..(nb_outputs * nb_hidden)).map(|_| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
+            bias_h: Matrix::new(nb_hidden, 1, (0..nb_hidden).map(|_| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>()),
+            bias_o: Matrix::new(nb_outputs, 1, (0..nb_outputs).map(|_| rand::thread_rng().gen_range(-1.0, 1.0)).collect::<Vec<f64>>())
+            
         }
     }
 
-    fn train(&mut self, inputs: Vec<f64>, targets: Vec<f64>) { 
+    fn train(&mut self, inputs: Matrix<f64>, targets: Matrix<f64>) { 
         // Feed forward inputs -> hidden
-        let mut hidden = Matrix::product(&self.weights_ih, &inputs);
-        hidden.add(&self.bias_h);
-        // Activation function
-        hidden.values = (0..hidden.values.len()).map(|y| (0..hidden.values[y].len()).map(|x| sigmoid(hidden.values[y][x])).collect()).collect();
+        let mut hidden: Matrix<f64> = &self.weights_ih * &inputs;
+        hidden = &hidden + &self.bias_h;
+        hidden = hidden.apply(&sigmoid);
 
         // Feed forward hidden -> outputs
-        let mut outputs = Matrix::product(&self.weights_ho, &hidden);
-        outputs.add(&self.bias_o);
-        // Activation function
-        outputs.values = (0..outputs.values.len()).map(|y| (0..outputs.values[y].len()).map(|x| sigmoid(outputs.values[y][x])).collect()).collect();
+        let mut outputs: Matrix<f64> = &self.weights_ho * &hidden;
+        outputs = &outputs + &self.bias_o;
+        outputs = outputs.apply(&sigmoid);
 
-        // Computing the outputs errors
-        let output_errors: Matrix = Matrix::sub(&targets, &outputs);
-
-        //outputs.values = (0..outputs.values.len()).map(|y| (0..outputs.values[y].len()).map(|x| dsigmoid(outputs.values[y][x])).collect()).collect();
-        //outputs.value = Matix::multiply();
+        // Computing outputs errors
+        let output_errors: Matrix<f64> = &targets - &outputs;
 
         // Computing hidden errors
-        let weights_ho_transposed: Matrix = Matrix::transpose(&self.weights_ho);
-        let hidden_errors: Matrix = Matrix::product(&weights_ho_transposed, &output_errors);
+        let weights_ho_transposed: Matrix<f64> = self.weights_ho.transpose();
+        let hidden_errors: Matrix<f64> = &weights_ho_transposed * &output_errors;
 
-        //println!("{:?}\n{:?}\n{:?}", outputs, targets, output_errors);
+        // println!("o: {:?}\nt: {:?}\ne: {:?}", outputs, targets, output_errors);
     }
 }
 
